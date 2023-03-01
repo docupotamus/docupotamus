6e36f8731931857dd58361689853275019cd677e
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-01 15:46:24 -0700

    Add Toolbar/Button component.

---
+++ packages/docusaurus-theme-common/src/theme/package/components/Toolbar/Button/index.tsx
@@ -0,0 +1,27 @@
+import IconButton from '@mui/material/IconButton';
+import * as React from 'react';
+// import { useToolbar } from '../../../contexts/toolbar';
+
+interface Props {
+    readonly children: React.ReactNode;
+    readonly tabId: string;
+};
+
+export default function Button(
+    {
+        children,
+        tabId,
+    }: Props
+): JSX.Element {
+    // const { setActiveTabId } = useToolbar();
+
+    const handleClick = () => {
+        // setActiveTabId(prev => (prev === tabId) ? '' : tabId);
+    };
+
+    return (
+        <IconButton onClick={handleClick}>
+            {children}
+        </IconButton>
+    );
+};
