188e2c8820d7675bdf74f252d92bfe63981074ac
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-02 13:02:39 -0700

    Add Workbench component.

---
+++ packages/docusaurus-theme-common/src/theme/package/components/Workbench/index.tsx
@@ -0,0 +1,94 @@
+import { useLocation } from '@docusaurus/router';
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import * as React from 'react';
+import { useToolbar } from '../../contexts/toolbar';
+import useCommonThemeConfig from '../../hooks/useCommonThemeConfig';
+import Loading from './Loading';
+// import styles from './styles.module.css';
+
+interface StyledBoxProps {
+    readonly workbenchIsOpen: boolean;
+    readonly boxShadowWidth: React.CSSProperties['width'];
+};
+
+const StyledBox = styled(Box, {
+    shouldForwardProp: (prop) => prop !== 'workbenchIsOpen' && prop !== 'boxShadowWidth',
+})<StyledBoxProps>(({ workbenchIsOpen, boxShadowWidth }) => ({
+    height: '100vh',
+
+    position: 'sticky',
+    top: 0,
+
+    display: workbenchIsOpen ? 'flex' : 'none',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+
+    background: `linear-gradient(
+        to right,
+        rgb(46, 69, 97) 0%,
+        rgb(39, 60, 85) 80%)`,
+    borderTopLeftRadius: 'var(--doc8-space-l-xl)',
+    paddingBottom: 'var(--doc8-space-xs)',
+    // TODO(dnguyen0304): Investigate refactoring to box-shadow style to reduce
+    // complexity.
+    '&::before': {
+        width: boxShadowWidth,
+        height: '100vh',
+
+        position: 'absolute',
+        top: 0,
+        left: `calc(-1 * ${boxShadowWidth})`,
+
+        content: '""',
+        background: `linear-gradient(
+            to right,
+            transparent,
+            rgba(60, 64, 67, 0.15) 70%,
+            rgba(60, 64, 67, 0.4) 100%)`,
+    },
+}));
+
+export default function Workbench(): JSX.Element {
+    const {
+        debug: {
+            loading: {
+                isEnabled: loadingIsEnabled,
+            },
+        },
+    } = useCommonThemeConfig();
+
+    const location = useLocation();
+    const { tabIdToConfig, activeTabId } = useToolbar();
+
+    const [isLoading, setIsLoading] = React.useState<boolean>(loadingIsEnabled);
+
+    // TODO(dnguyen0304): Add error handling.
+    const Tab = activeTabId && tabIdToConfig.get(activeTabId)?.Component;
+
+    React.useEffect(() => {
+        if (!loadingIsEnabled) {
+            return;
+        }
+        setIsLoading(true);
+    }, [location]);
+
+    return (
+        // TODO(dnguyen0304): Change boxShadowWidth to custom property.
+        <StyledBox
+            // className={isLoading ? styles.workbench_container__load : ''}
+            workbenchIsOpen={!!activeTabId}
+            boxShadowWidth='var(--doc8-space-xs)'
+        >
+            {/* TODO(dnguyen0304): Replace temporary placeholder stub. */}
+            <React.Suspense fallback={<p>Loading...</p>}>
+                {
+                    // TODO(dnguyen0304): Fix loading not triggering on open.
+                    isLoading
+                        ? <Loading setIsLoading={setIsLoading} />
+                        : Tab && <Tab />
+                }
+            </React.Suspense>
+        </StyledBox >
+    );
+};
