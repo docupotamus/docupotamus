314aa0db467e24769ba0bdd5fc878eb5add776c2
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-02 03:04:54 -0700

    Add tabs to ThemeConfigSchema for validateThemeConfig.

--- packages/docusaurus-theme-common/src/validateThemeConfig.ts
+++ packages/docusaurus-theme-common/src/validateThemeConfig.ts
@@ -11,11 +11,29 @@ declare module '@docusaurus/types' {
     }
 };
 
-const DEFAULT_THEME_CONFIG: CommonThemeConfig = {};
+const DEFAULT_THEME_CONFIG: CommonThemeConfig = {
+    tabs: [],
+};
+
+const TabSchema = Joi.object({
+    tabId: Joi
+        .string()
+        .required(),
+    modulePath: Joi
+        .string()
+        .required(),
+    iconModulePath: Joi
+        .string()
+        .required(),
+});
 
 // TODO(dnguyen0304): Investigate missing labels.
 export const ThemeConfigSchema = Joi.object<ThemeConfig>({
     docupotamusCommon: Joi.object({
+        tabs: Joi
+            .array()
+            .items(TabSchema)
+            .default(DEFAULT_THEME_CONFIG.tabs),
     })
         .label('themeConfig.docupotamusCommon')
         .default(DEFAULT_THEME_CONFIG),
