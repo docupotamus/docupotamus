251ded1bc9f0c8dab66d4f863e9ba0c7da305b67
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-04 09:56:20 -0700

    [common] Add Toolbar/Entry component.

---
+++ packages/docusaurus-theme-common/src/theme/package/components/Toolbar/Entry/index.tsx
@@ -0,0 +1,17 @@
+// This is a sentinel to be swizzled through wrapping. It is guaranteed to be a
+// descendant component of the ToolbarProvider and therefore has always access
+// to the context.
+
+import * as React from 'react';
+
+interface Props {
+    readonly children: React.ReactNode;
+};
+
+export default function Entry({ children }: Props): JSX.Element {
+    return (
+        <>
+            {children}
+        </>
+    );
+};
