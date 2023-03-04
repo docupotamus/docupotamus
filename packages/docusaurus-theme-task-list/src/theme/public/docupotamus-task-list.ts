9fa2d09657e3e08ff404bea7aa200ff89f68049d
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-04 13:15:17 -0700

    [task-list] Change to directory for docupotamus-task-list public.

--- packages/docusaurus-theme-task-list/src/theme/public/docupotamus-task-list.ts
+++
@@ -1,4 +0,0 @@
-export { default as WorkbenchIcon } from '@mui/icons-material/TaskAltOutlined';
-export { default as TaskList } from '../../theme/package/components/TaskList';
-export { default as RootDecorator } from '../decorators/Root';
-export { default as WorkbenchTab } from '../package/components/WorkbenchTab';
