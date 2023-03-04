7171ebe2c3d52dec1a93083a1c9bee82f0ef5f6b
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-04 13:23:32 -0700

    [preset] Add Toolbar/Entry swizzle for plugin.

---
+++ packages/docusaurus-preset-classic/src/plugin/theme/docupotamus-common/Toolbar/Entry/index.tsx
@@ -0,0 +1,28 @@
+import type { WrapperProps } from '@docusaurus/types';
+import ToolbarEntryInit from '@theme-init/docupotamus-common/Toolbar/Entry';
+import { useToolbar } from '@theme/docupotamus-common';
+import type ToolbarEntryType from '@theme/docupotamus-common/Toolbar/Entry';
+import { WorkbenchIcon, WorkbenchTab } from '@theme/docupotamus-task-list';
+import * as React from 'react';
+
+type Props = Readonly<WrapperProps<typeof ToolbarEntryType>>;
+
+export default function ToolbarEntryWrapper(props: Props): JSX.Element {
+    const { dispatchTabIdToConfig } = useToolbar();
+
+    React.useEffect(() => {
+        dispatchTabIdToConfig({
+            type: 'setTab',
+            tabId: 'task-list',
+            newValue: {
+                tabId: 'task-list',
+                Component: <WorkbenchTab />,
+                IconComponent: <WorkbenchIcon />,
+            },
+        })
+    }, []);
+
+    return (
+        <ToolbarEntryInit {...props} />
+    );
+};
