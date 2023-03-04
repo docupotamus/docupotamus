21af4bbc72c4ab3eda761a42f19e4b8bcd1912fa
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-04 10:00:06 -0700

    [common] Change from file to directory for docupotamus-common public.

--- packages/docusaurus-theme-common/src/theme/public/docupotamus-common.ts
+++
@@ -1,4 +0,0 @@
-export { default as DocPageLayoutDecorator } from '../decorators/DocPage/Layout';
-export { default as DocPageLayoutMainDecorator } from '../decorators/DocPage/Layout/Main';
-export { default as RootDecorator } from '../decorators/Root';
-export { useToolbar } from '../package/contexts/toolbar';
---
+++ packages/docusaurus-theme-common/src/theme/public/docupotamus-common/index.tsx
@@ -0,0 +1,4 @@
+export { default as DocPageLayoutDecorator } from '../../decorators/DocPage/Layout';
+export { default as DocPageLayoutMainDecorator } from '../../decorators/DocPage/Layout/Main';
+export { default as RootDecorator } from '../../decorators/Root';
+export { useToolbar } from '../../package/contexts/toolbar';
