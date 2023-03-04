03c13854427c50911d442aa931d0bdbd76eceba4
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-04 06:54:28 -0700

    [common] Remove keyByTabId for toolbar context.

--- packages/docusaurus-theme-common/src/theme/package/contexts/toolbar.tsx
+++ packages/docusaurus-theme-common/src/theme/package/contexts/toolbar.tsx
@@ -1,12 +1,6 @@
 import { ReactContextError } from '@docupotamus/docusaurus-lib-common/contexts';
 import * as React from 'react';
 
-interface BaseTabConfig {
-    readonly tabId: string;
-    readonly modulePath: string;
-    readonly iconModulePath: string;
-}
-
 interface TabConfig {
     readonly Component: React.LazyExoticComponent<() => JSX.Element>;
     readonly IconComponent: React.LazyExoticComponent<() => JSX.Element>;
@@ -14,40 +8,20 @@ interface TabConfig {
 
 interface TabIdToConfig extends ReadonlyMap<string, TabConfig> { };
 
-const tabConfigs: readonly BaseTabConfig[] = [];
-
-// TODO(dnguyen0304): Add real implementation.
-//   See: https://stackoverflow.com/a/47956054
-//   See: https://stackoverflow.com/a/58350377
-//   See: https://github.com/facebook/react/issues/14603
-const keyByTabId = (tabConfigs: readonly BaseTabConfig[]): TabIdToConfig => {
-    return new Map(
-        [[
-            'task-list',
-            {
-                Component: React.lazy(() => import(
-                    '@theme/docupotamus-task-list'
-                ).then(module => ({ default: module.WorkbenchTab }))),
-                IconComponent: React.lazy(() => import(
-                    '@theme/docupotamus-task-list'
-                ).then(module => ({ default: module.WorkbenchIcon }))),
-            },
-        ]],
-        // tabConfigs.map(tabConfig => [
-        //     tabConfig.tabId,
-        //     {
-        //         Component: React.lazy(() =>
-        //             import(`${tabConfig.modulePath}`)
-        //                 .then(module => ({ default: module.WorkbenchTab }))
-        //         ),
-        //         IconComponent: React.lazy(() =>
-        //             import(`${tabConfig.iconModulePath}`)
-        //                 .then(module => ({ default: module.WorkbenchIcon }))
-        //         ),
-        //     },
-        // ])
-    );
-};
+// TODO(dnguyen0304): Investigate dynamic instead of hard-coded configuration.
+const tabIdToConfig: TabIdToConfig = new Map(
+    [[
+        'task-list',
+        {
+            Component: React.lazy(() => import(
+                '@theme/docupotamus-task-list'
+            ).then(module => ({ default: module.WorkbenchTab }))),
+            IconComponent: React.lazy(() => import(
+                '@theme/docupotamus-task-list'
+            ).then(module => ({ default: module.WorkbenchIcon }))),
+        },
+    ]],
+);
 
 interface ContextValue {
     readonly tabIdToConfig: TabIdToConfig;
@@ -59,8 +33,6 @@ const Context = React.createContext<ContextValue | undefined>(undefined);
 
 const useContextValue = (): ContextValue => {
     const [activeTabId, setActiveTabId] = React.useState<string>('');
-
-    const tabIdToConfig = keyByTabId(tabConfigs);
 
     return React.useMemo(
         () => ({
