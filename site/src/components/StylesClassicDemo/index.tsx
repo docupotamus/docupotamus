1859b10ce048fadd9588a9e849909d931c76a62b
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-07 09:10:01 -0700

    [site] Add marginVertical Slider for StylesClassicDemo component.

--- site/src/components/StylesClassicDemo/index.tsx
+++ site/src/components/StylesClassicDemo/index.tsx
@@ -5,14 +5,16 @@ import { styled } from '@mui/material/styles';
 import * as React from 'react';
 
 const backgroundColorDefault: string = '--ifm-color-primary';
+const marginVerticalDefault: string = 'space-l';
 
 interface LayoutProps {
     readonly backgroundColor: string;
+    readonly marginVertical: React.CSSProperties['margin'];
 };
 
 const Layout = styled(Box, {
-    shouldForwardProp: (prop) => prop !== 'backgroundColor'
-})<LayoutProps>(({ backgroundColor }) => ({
+    shouldForwardProp: (prop) => prop !== 'backgroundColor' && prop !== 'marginVertical'
+})<LayoutProps>(({ backgroundColor, marginVertical }) => ({
     '& > .MuiBox-root': {
         width: '100%',
         display: 'grid',
@@ -22,7 +24,7 @@ const Layout = styled(Box, {
     },
     '& > .MuiBox-root > h2': {
         color: 'var(--ifm-color-content-inverse)',
-        margin: 'var(--doc8-space-l) 0',
+        margin: `var(--doc8-${marginVertical}) 0`,
     },
 }));
 
@@ -44,12 +46,37 @@ const backgroundColorMarks: Mark[] = [
         label: '--ifm-color-danger',
     },
 ];
+const marginVerticalMarks: Mark[] = [
+    {
+        value: 0 * 10,
+        label: 'space-xs',
+    },
+    {
+        value: 1 * 10,
+        label: 'space-s',
+    },
+    {
+        value: 2 * 10,
+        label: 'space-m',
+    },
+    {
+        value: 3 * 10,
+        label: marginVerticalDefault,
+    },
+    {
+        value: 4 * 10,
+        label: 'space-xl',
+    },
+];
 
 const backgroundColorMapping = toMapping(backgroundColorMarks);
+const marginVerticalMapping = toMapping(marginVerticalMarks);
 
 export default function StylesClassicDemo(): JSX.Element {
     const [backgroundColor, setBackgroundColor] =
         React.useState<string>(backgroundColorDefault);
+    const [marginVertical, setMarginVertical] =
+        React.useState<React.CSSProperties['margin']>(marginVerticalDefault);
 
     const handleBackgroundColorChange = (
         _: Event,
@@ -61,8 +88,21 @@ export default function StylesClassicDemo(): JSX.Element {
         }
     };
 
+    const handleMarginVerticalChange = (
+        _: Event,
+        value: number | number[],
+    ) => {
+        const newValue = marginVerticalMapping.get(value as number);
+        if (newValue) {
+            setMarginVertical(newValue);
+        }
+    };
+
     return (
-        <Layout backgroundColor={backgroundColor}>
+        <Layout
+            backgroundColor={backgroundColor}
+            marginVertical={marginVertical}
+        >
             <Box>
                 <h2>doc8</h2>
             </Box>
@@ -71,6 +111,15 @@ export default function StylesClassicDemo(): JSX.Element {
                 step={50}
                 marks={backgroundColorMarks}
                 onChange={handleBackgroundColorChange}
+                size='small'
+                valueLabelDisplay='off'
+            />
+            <Slider
+                defaultValue={30}
+                step={10}
+                max={(marginVerticalMarks.length - 1) * 10}
+                marks={marginVerticalMarks}
+                onChange={handleMarginVerticalChange}
                 size='small'
                 valueLabelDisplay='off'
             />
