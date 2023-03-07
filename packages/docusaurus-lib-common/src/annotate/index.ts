575b9c7adb6938d1c0795296b87b5928f75591f6
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-07 03:07:49 -0700

    [common] Fix untyped module build errors for annotate directory.

--- packages/docusaurus-lib-common/src/annotate/index.ts
+++ packages/docusaurus-lib-common/src/annotate/index.ts
@@ -1,7 +1,11 @@
 import * as rangeUtils from './utils/range';
 
+// TODO(dnguyen0304): Fix untyped module.
+// @ts-ignore
 export { RangeAnchor } from './anchor/types';
 export { highlightRange } from './highlight';
+// TODO(dnguyen0304): Fix untyped module.
+// @ts-ignore
 export { SelectionObserver } from './selection-observer';
 export { SelectionTopCenterPositioner } from './tooltip/positioner';
 export {
