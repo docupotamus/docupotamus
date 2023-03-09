2e21adf5ac901e6710fb25c51acb798c828b45e3
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-09 16:04:20 -0700

    [env-var] Change to use local implementation for CodeBlock/Line swizzle.

--- packages/docusaurus-theme-environment-variables/src/theme/swizzle/CodeBlock/Line/index.tsx
+++ packages/docusaurus-theme-environment-variables/src/theme/swizzle/CodeBlock/Line/index.tsx
@@ -1,12 +1,12 @@
 import type { WrapperProps } from '@docusaurus/types';
-import CodeBlockLineInit from '@theme-init/CodeBlock/Line';
 import type CodeBlockLineType from '@theme/CodeBlock/Line';
 import * as React from 'react';
+import CodeBlockLine from '../../../package/components/CodeBlock/Line';
 
 type Props = Readonly<WrapperProps<typeof CodeBlockLineType>>;
 
 export default function CodeBlockLineWrapper(props: Props): JSX.Element {
     return (
-        <CodeBlockLineInit {...props} />
+        <CodeBlockLine {...props} />
     );
 };
