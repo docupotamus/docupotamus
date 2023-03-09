d3b3a736c1fe4a78cffee1f9144dfd4dca1458cc
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-09 16:29:39 -0700

    Add WorkbenchIcon for public.

--- packages/docusaurus-theme-environment-variables/src/theme/public/docupotamus-environment-variables/index.ts
+++ packages/docusaurus-theme-environment-variables/src/theme/public/docupotamus-environment-variables/index.ts
@@ -1,2 +1,3 @@
+export { default as WorkbenchIcon } from '@mui/icons-material/Code';
 export { default as WorkbenchTab } from '../../package/components/WorkbenchTab';
 export { default as CodeBlockLine } from '../../swizzle/CodeBlock/Line';
