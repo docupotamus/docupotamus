6ac4c3eff594df62136b73624199322f3fbda6ca
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-10 14:34:16 -0700

    [env-var] Fix size for Workbench/CopyButton styles.

--- packages/docusaurus-theme-environment-variables/src/theme/package/components/WorkbenchTab/CopyButton/index.tsx
+++ packages/docusaurus-theme-environment-variables/src/theme/package/components/WorkbenchTab/CopyButton/index.tsx
@@ -1,19 +1,9 @@
 import type { WrapperProps } from '@docusaurus/types';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
 import Tooltip from '@mui/material/Tooltip';
 import CopyButtonInit from '@theme-original/CodeBlock/CopyButton';
 import type CopyButtonType from '@theme/CodeBlock/CopyButton';
 import * as React from 'react';
 import styles from './styles.module.css';
-
-// TODO(dnguyen0304): Investigate adding border styles similar to how
-//   @theme/CodeBlock does.
-const StyledBox = styled(Box)({
-    width: 'var(--doc8-space-l)',
-    aspectRatio: '1 / 1',
-    alignSelf: 'center',
-});
 
 type Props = Pick<WrapperProps<typeof CopyButtonType>, 'code'>;
 
@@ -23,12 +13,12 @@ export default function CopyButton({ code }: Props): JSX.Element {
             placement='top'
             title='Copy last active'
         >
-            <StyledBox>
+            <div className={styles.CopyButton_layout}>
                 <CopyButtonInit
                     className={styles.CopyButton}
                     code={code}
                 />
-            </StyledBox>
+            </div>
         </Tooltip>
     );
 };
