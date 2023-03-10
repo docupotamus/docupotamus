15c72e552b894549d962c46666734f5e3c25a4c9
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-10 03:44:01 -0700

    [env-var] Document difference after ejecting for CodeBlock/Line component.

--- packages/docusaurus-theme-environment-variables/src/theme/package/components/CodeBlock/Line/index.tsx
+++ packages/docusaurus-theme-environment-variables/src/theme/package/components/CodeBlock/Line/index.tsx
@@ -25,6 +25,7 @@ export default function CodeBlockLine({
     className: clsx(classNames, showLineNumbers && styles.codeLine),
   });
 
+  // This is the only difference after ejecting.
   const lineTokens = (
     <PartitionedLineTokens
         line={line}
