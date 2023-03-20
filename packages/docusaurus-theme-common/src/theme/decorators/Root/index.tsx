b83afec6a6efca3b921b1a349bf097447f324249
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-20 15:43:31 -0600

    [common] Add MarkdownProvider for Root decorator.

--- packages/docusaurus-theme-common/src/theme/decorators/Root/index.tsx
+++ packages/docusaurus-theme-common/src/theme/decorators/Root/index.tsx
@@ -1,5 +1,6 @@
 import ToolbarEntry from '@theme/docupotamus-common/Toolbar/Entry';
 import * as React from 'react';
+import { MarkdownProvider } from '../../package/contexts/markdown';
 import { ToolbarProvider } from '../../package/contexts/toolbar';
 import '../../package/styles.css';
 
@@ -9,10 +10,12 @@ interface Props {
 
 export default function RootDecorator({ children }: Props): JSX.Element {
     return (
-        <ToolbarProvider>
-            <ToolbarEntry>
-                {children}
-            </ToolbarEntry>
-        </ToolbarProvider>
+        <MarkdownProvider>
+            <ToolbarProvider>
+                <ToolbarEntry>
+                    {children}
+                </ToolbarEntry>
+            </ToolbarProvider>
+        </MarkdownProvider>
     );
 };
