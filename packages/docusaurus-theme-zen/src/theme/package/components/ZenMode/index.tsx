d00999db164809059d279c1e6ad1ee91ab1242bb
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-20 11:36:45 -0600

    [zen] Add Intersector to ZenMode component.

--- packages/docusaurus-theme-zen/src/theme/package/components/ZenMode/index.tsx
+++ packages/docusaurus-theme-zen/src/theme/package/components/ZenMode/index.tsx
@@ -5,8 +5,11 @@ import * as React from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
 
 const CLASS_NAME: string = 'zen-mode';
+const MOUSE_RADIUS: React.CSSProperties['width'] = '10vw';
 
 const StyledBox = styled(Box)({
+    position: 'relative',
+
     [`&.${CLASS_NAME} *`]: {
         // TODO(dnguyen0304): Fix overriding smaller opacity values.
         opacity: 0.8,
@@ -17,14 +20,39 @@ const StyledBox = styled(Box)({
     },
 });
 
+const Intersector = styled(Box)({
+    width: `calc(2 * ${MOUSE_RADIUS})`,
+    height: `calc(2 * ${MOUSE_RADIUS})`,
+
+    position: 'absolute',
+
+    borderRadius: '50%',
+    translate: `-${MOUSE_RADIUS} -${MOUSE_RADIUS}`,
+
+    // TODO(dnguyen0304): Remove development code.
+    backgroundColor: 'red',
+    zIndex: 9999999,
+});
+
 interface Props {
     children: React.ReactNode;
 };
 
 export default function ZenMode({ children }: Props): JSX.Element {
     const [isEnabled, setIsEnabled] = React.useState<boolean>(false);
+    const [intersectorTop, setIntersectorTop] =
+        React.useState<React.CSSProperties['top']>(0);
+    const [intersectorLeft, setIntersectorLeft] =
+        React.useState<React.CSSProperties['left']>(0);
 
     const toggleIsEnabled = () => setIsEnabled(prev => !prev);
+
+    const handleMouseMove = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    ) => {
+        setIntersectorTop(`${event.clientY}px`);
+        setIntersectorLeft(`${event.clientX}px`);
+    };
 
     useHotkeys(
         "shift",
@@ -46,8 +74,15 @@ export default function ZenMode({ children }: Props): JSX.Element {
     );
 
     return (
-        <StyledBox className={clsx({ [`${CLASS_NAME}`]: isEnabled })}>
+        <StyledBox
+            className={clsx({ [`${CLASS_NAME}`]: isEnabled })}
+            onMouseMove={handleMouseMove}
+        >
             {children}
+            <Intersector style={{
+                top: intersectorTop,
+                left: intersectorLeft,
+            }} />
         </StyledBox>
     );
 };
