4a91bddee2839b7d96522c0e5cf2f72469b08c7c
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-22 14:38:20 -0600

    [src-hook] Add RawContentProvider for DocPage/Layout decorator.

--- packages/docusaurus-plugin-content-docs-src-hook/src/theme/decorator/DocPage/Layout/index.tsx
+++ packages/docusaurus-plugin-content-docs-src-hook/src/theme/decorator/DocPage/Layout/index.tsx
@@ -1,17 +1,19 @@
 import type { WrapperProps } from '@docusaurus/types';
 import type DocPageLayoutType from '@theme/DocPage/Layout';
 import * as React from 'react';
+import { RawContentProvider } from '../../../package/contexts/rawContent';
 
 type Props = Readonly<WrapperProps<typeof DocPageLayoutType>>;
 
 export default function DocPageLayoutDecorator(
     {
+        rawContent,
         children,
     }: Props
 ): JSX.Element {
     return (
-        <>
+        <RawContentProvider rawContent={rawContent}>
             {children}
-        </>
+        </RawContentProvider>
     );
 };
