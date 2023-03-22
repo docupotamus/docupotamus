9cd586a2d3d45a4ebb76b9c80d5f1df4527644dd
Duy Nguyen <dnguyen0304@gmail.com>
2023-03-22 11:00:43 -0600

    [src-hook] Update comments for index.

--- packages/docusaurus-plugin-content-docs-src-hook/src/index.ts
+++ packages/docusaurus-plugin-content-docs-src-hook/src/index.ts
@@ -7,7 +7,7 @@ import type { LoadContext, Plugin, RouteConfig } from '@docusaurus/types';
 import { docuHash } from '@docusaurus/utils';
 import fs from 'fs/promises';
 
-// See https://github.com/facebook/docusaurus/blob/01ac2e0fcaccaf469992f93a0e8bf04e61cf850e/packages/docusaurus-utils/src/pathUtils.ts#L93
+// See: https://github.com/facebook/docusaurus/blob/01ac2e0fcaccaf469992f93a0e8bf04e61cf850e/packages/docusaurus-utils/src/pathUtils.ts#L93
 const ALIASED_SITE_PATH_PREFIX: string = '@site';
 
 export default async function pluginContentDocsSrcHook(
@@ -82,7 +82,7 @@ export default async function pluginContentDocsSrcHook(
 
         // TODO(dnguyen0304): Investigate how to specify a different plugin name
         // There are hard-coded references to specific plugin names.
-        // See https://github.com/facebook/docusaurus/blob/7ab2bd32342496f3f7373bc67d03a0da0eeffa40/packages/docusaurus-plugin-content-docs/src/client/index.ts#L89
+        // See: https://github.com/facebook/docusaurus/blob/7ab2bd32342496f3f7373bc67d03a0da0eeffa40/packages/docusaurus-plugin-content-docs/src/client/index.ts#L89
         name: wrappedPlugin.name,
 
         extendCli(cli) {
